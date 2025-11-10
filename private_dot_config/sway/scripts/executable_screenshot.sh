641d8f95dfd8654b337c776dd5e00fe49c0cec48
James Sejer Canham <jsc@vertica.dk>
2025-11-10 12:47:45 +0100

    screenshotting

---
+++ private_dot_config/sway/scripts/executable_screenshot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+case $1 in
+    "full")
+        grim ~/Pictures/screenshots/screenshot-$(date +%Y%m%d-%H%M%S).png
+        notify-send "Screenshot" "Full screen captured"
+        ;;
+    "full-clipboard")
+        grim - | wl-copy
+        notify-send "Screenshot" "Full screen copied to clipboard"
+        ;;
+    "area")
+        grim -g "$(slurp)" ~/Pictures/screenshots/screenshot-$(date +%Y%m%d-%H%M%S).png
+        notify-send "Screenshot" "Area captured"
+        ;;
+    "area-clipboard")
+        grim -g "$(slurp)" - | wl-copy
+        notify-send "Screenshot" "Area copied to clipboard"
+        ;;
+    "area-edit")
+        grim -g "$(slurp)" - | swappy -f -
+        ;;
+    "full-edit")
+        grim - | swappy -f -
+        ;;
+    "window")
+        # Get focused window geometry
+        WINDOW=$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')
+        grim -g "$WINDOW" ~/Pictures/screenshots/screenshot-$(date +%Y%m%d-%H%M%S).png
+        notify-send "Screenshot" "Window captured"
+        ;;
+esac
